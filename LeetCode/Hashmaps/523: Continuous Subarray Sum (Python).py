eacee0d5666a5f3220fad93e3f7064d706919155
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-07-22 20:50:28 -0500

    Create 523: Continuous Subarray Sum (Python).py

---
+++ LeetCode/Hashmaps/523: Continuous Subarray Sum (Python).py
@@ -0,0 +1,11 @@
+remainder = {0: -1} #map remainder -> end index
+        total = 0
+
+        for i, n in enumerate(nums):
+            total += n
+            r = total % k
+            if r not in remainder:
+                remainder[r] = i
+            elif i - remainder[r] > 1:
+                return True
+        return False
