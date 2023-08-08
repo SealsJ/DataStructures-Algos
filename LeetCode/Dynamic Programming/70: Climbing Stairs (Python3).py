fe4e4f276ac18ea0d20a9eae285e68110bbde3d4
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-08-08 16:47:44 -0500

    Create 70: Climbing Stairs (Python3).py

---
+++ LeetCode/Dynamic Programming/70: Climbing Stairs (Python3).py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #Initialize two variables to keep track of subproblems
+        one, two = 1, 1
+
+        #Iterate through list of numbers to not go out of bounds
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+
+        return one
