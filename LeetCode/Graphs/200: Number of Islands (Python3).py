fea552581029f0e293a253b8269aeea8f9417826
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-09-12 13:12:04 -0500

    Create 200: Number of Islands (Python3).py

---
+++ LeetCode/Graphs/200: Number of Islands (Python3).py
@@ -0,0 +1,47 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        #BFS GRAPH ALGO TRAVERSAL
+
+        #Consider the edge case if no grid
+        if not grid:
+            return 0
+
+        #Figure out dimensions of the grid
+        rows, cols = len(grid), len(grid[0])
+
+        #Implement a set to keep track of 'islands' (1s) already visited / count islands
+        visit = set()
+        islands = 0
+
+        #Create BFS function to determine where an island stops
+        def bfs(r,c):
+            #Iterative approach, using a queue data structure for memory
+            q = collections.deque()
+            visit.add((r,c))
+            q.append((r,c))
+
+            #Keeping track of island size by using conditions
+            while q:
+                row, col = q.popleft()
+                #possible directions
+                directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
+                #iterate through directions
+                for dr, dc in directions:
+                    r,c = row + dr, col + dc
+                    #Check if in bounds
+                    if (r in range(rows) and c in range(cols) and
+                    grid[r][c] == "1" and (r,c) not in visit):
+                        #We have to now run bfs on this island (Add to queue)
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        #Need to traverse every row/column to keep track of 1s
+        for r in range(rows):
+            for c in range(cols):
+                #Increment only if 1 hasn't been visited before
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)
+                    islands += 1
+
+        return islands 
