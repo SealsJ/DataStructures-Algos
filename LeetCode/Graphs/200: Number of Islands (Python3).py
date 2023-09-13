905045eb0fb97b08b33b404a5595c64a52236b8d
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-09-13 15:49:06 -0500

    Update 200: Number of Islands (Python3).py

--- LeetCode/Graphs/200: Number of Islands (Python3).py
+++ LeetCode/Graphs/200: Number of Islands (Python3).py
@@ -1,6 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #BFS GRAPH ALGO TRAVERSAL
+        #BFS GRAPH ALGO TRAVERSAL: TIME COMPLEXITY -> O(M * N)
 
         #Consider the edge case if no grid
         if not grid:
@@ -45,3 +45,31 @@ class Solution:
                     islands += 1
 
         return islands 
+
+
+        #DFS SOLUTION
+        if not grid:
+            return 0
+
+        visit = set()
+
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r,c):
+            if (r not in range(rows) or c not in range(cols) or grid[r][c] == "0" or (r,c) in visit):
+                return
+                
+            visit.add((r,c))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+
+        islands = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c ) not in visit:
+                    islands += 1
+                    dfs(r,c)
+
+        return islands
