e8c7498e4d62c6d319bec0458657e5450d001267
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-09-15 17:07:30 -0500

    Create stack(Updated).py

---
+++ DSA Code/stack(Updated).py
@@ -0,0 +1,50 @@
+"""
+Stack: Maintains a last-in, first-out (LIFO) order
+
+Time Complexity:
+Push(insertion) -> O(1) .append() in Python
+Pop(Deletion) -> O(1) .pop() in Python
+Peek(Look at top element) -> O(1) // Make function to look at list[-1]
+Search -> O(n) in the worst case where n is the # of elements in the stack
+
+Space Complexity:
+O(n) where n is the # of elements in the stack. Accounts for memory to store stack
+"""
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            raise Exception("Stack is empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise Exception("Stack is empty")
+
+    def size(self):
+        return len(self.items)
+
+# Example usage:
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print("Stack size:", stack.size())
+print("Top element:", stack.peek())
+
+popped_item = stack.pop()
+print("Popped item:", popped_item)
+print("Stack size after pop:", stack.size())
