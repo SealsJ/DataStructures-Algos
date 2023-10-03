3a52255fe8bafd85a3a5c2dd749756aac4c661e7
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-10-03 16:34:38 -0500

    Update 347. Top K Frequent Elements (Python).py

--- LeetCode/Arrays/347. Top K Frequent Elements (Python).py
+++ LeetCode/Arrays/347. Top K Frequent Elements (Python).py
@@ -1,21 +1,25 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        #Bucket Sort Algo
-
-        #Initiate Hashmap to count frequency
-        count = {}
-        freq =[[] for i in range(len(nums) + 1)]
-
-        #Count how many time each # occurs in nums
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #Create a hashmap to count number of occurences for a specific number
+        hashmap = {}
+        #Create buckets, for potential frequency a number can occur up to length of nums
+        freq = [[] for i in range(len(nums) + 1)]
+        
+        #Count frequency of number in nums and store count in hashmap
         for n in nums:
-            count[n] = 1 + count.get(n,0)
-        for n, c in count.items():
+            hashmap[n] = 1 + hashmap.get(n, 0)
+        #Take the numbers and append them to where they occured in terms of frequency
+        for n, c in hashmap.items():
             freq[c].append(n)
 
-        #return k elements in descending order
+        #Create result variable
         res = []
+        #Iterate through freq in descending order return k elements
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
+
+
+        #TC: O(N) & SC: O(N) /// Bucket Sort Algo 
