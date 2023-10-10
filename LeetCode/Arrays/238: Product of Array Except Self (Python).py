fdc9b0ccc18c053a87b48404ef269dcf48502f6d
Jeremy Seals <106201853+SealsJ@users.noreply.github.com>
2023-10-10 17:48:17 -0500

    Update 238: Product of Array Except Self (Python).py

--- LeetCode/Arrays/238: Product of Array Except Self (Python).py
+++ LeetCode/Arrays/238: Product of Array Except Self (Python).py
@@ -16,3 +16,19 @@ class Solution(object):
             postfix *= nums[i]
 
         return result
+
+
+        #Second Answer in O(n) TC
+        class Solution:
+        def productExceptSelf(self, nums: List[int]) -> List[int]:
+            length = len(nums)
+            products = [1] * length
+            for i in range(1, length):
+                products[i] = products[i-1] * nums[i-1]
+    
+            right = nums[-1]
+            for i in range(length-2, -1, -1):
+                products[i] *= right
+                right *= nums[i]
+            
+            return products
